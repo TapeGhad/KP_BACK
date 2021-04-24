dde02b192753115285d430789c5ccf276908ba4f
tapeghad <tapeghad@gmail.com>
2021-04-24 22:06:08 +0300

    fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const db = require("./context")(mongoose);
 const server = require("./server")(db);
 const sockets = require("./global-controllers/socket");
 
-const app = server.listen(4000, () => {
+const app = server.listen(process.env.PORT || 4000, () => {
   console.log("Running")
   sockets.sockets(app, db);
 });
